c828c52e941406e7d7442d4e269b333188738a30
KUSHAL H <56520530+kushal-h@users.noreply.github.com>
2020-10-04 23:36:34 +0530

    security update

--- DRF_intro/settings.py
+++ DRF_intro/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+from secret import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e@w!*f5g%xs(j&x3cuy*n5tg^b!t_sb!$f86q#unn&n!t2t_xf'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
