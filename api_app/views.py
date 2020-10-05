e703b862eab308dc83089f5c86375ec6b582753c
KUSHAL H <56520530+kushal-h@users.noreply.github.com>
2020-10-05 22:12:14 +0530

    App views written

--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Article
+from .serializers import  Articleserializers
 
 # Create your views here.
+
+def article_list(request):
+
+    if request.methods == 'GET':
+        articles = Article.objects.all()
+        serializer = Articleserializers(articles, many=True)
+        return JsonResponse(serilaizer.data, safe= False)
+
+    elif request.methods == 'POST':
+        data = JSONParser().parse(request)
+        serializer = Articleserializers(data= data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
