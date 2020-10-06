2e7b7fafc897b47e815f78ccec4968c687a99caf
KUSHAL H <56520530+kushal-h@users.noreply.github.com>
2020-10-06 19:44:58 +0530

    writing views by class for details

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import article_list, artilce_detail, ArticleARIViews
+from .views import article_list, artilce_detail, ArticleARIViews, ArticleDetails
 
 urlpatterns = [
    # path('article/', article_list),
-    path('detail/<int:pk>/', artilce_detail),
-    path('articles/', ArticleARIViews.as_view())
+    #path('detail/<int:pk>/', artilce_detail),
+    path('articles/', ArticleARIViews.as_view()),
+    path('details/<int:id>/', ArticleDetails.as_view())
 ]
