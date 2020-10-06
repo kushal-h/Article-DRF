2c555f03ea1e369303ce65768a0f096cede2c7e4
KUSHAL H <56520530+kushal-h@users.noreply.github.com>
2020-10-06 22:02:56 +0530

    writing views by using generics and mixins

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import article_list, artilce_detail, ArticleARIViews, ArticleDetails
+from .views import article_list, artilce_detail, ArticleARIViews, ArticleDetails, GenericAPIView
 
 urlpatterns = [
    # path('article/', article_list),
     #path('detail/<int:pk>/', artilce_detail),
     path('articles/', ArticleARIViews.as_view()),
-    path('details/<int:id>/', ArticleDetails.as_view())
+    path('details/<int:id>/', ArticleDetails.as_view()),
+    path('generic/articles/<idt:id>/', GenericAPIView.as_view()),
 ]
